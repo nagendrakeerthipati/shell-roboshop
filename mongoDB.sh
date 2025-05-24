9113e5a24f597f2a65ee5f9da05797bef49ddf85
nagendrakeerthipati <nagendravarma.nv@gmail.com>
2025-05-24 20:10:23 +0530

    automation of roboshop

---
+++ mongoDB.sh
@@ -0,0 +1,43 @@
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
+        echo -e " $2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e " $2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copied or not "
+
+dnf install mongodb-org -y
+
+systemctl enable mongod
+systemctl start mongod
+
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+VALIDATE $? "editing was..."
+
+systemctl restart mongod
