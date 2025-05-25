43b52706bc0ce8ccc823584dc68906a84ca15fa2
nagendrakeerthipati <nagendravarma.nv@gmail.com>
2025-05-25 21:23:57 +0530

    automation using shell

--- catalogue.sh
+++ catalogue.sh
@@ -68,7 +68,7 @@ VALIDATE $? "npm install "
 cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
 VALIDATE $? "Copying from catalogue"
 
-sed -i '/s/<MONGODB-SERVER-IPADDRESS>/mongodb.nagendrablog.site/' catalogue.service &>>$LOG_FILE
+sed -i 's/<MONGODB-SERVER-IPADDRESS>/mongodb.nagendrablog.site/' catalogue.service &>>$LOG_FILE
 VALIDATE $? "replaceing ip address with site name "
 
 systemctl daemon-reload &>>$LOG_FILE
