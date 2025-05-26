4c4f3a0027359d8af61ffdb4ac199a7648118fc1
nagendrakeerthipati <nagendravarma.nv@gmail.com>
2025-05-26 21:51:37 +0530

    automation using shell

---
+++ user.sh
@@ -0,0 +1,69 @@
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
+        echo -e " $2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e " $2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable nodejs -y &>>LOG_FILE
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "installing nodejs "
+
+id roboshop
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "Roboshop user created "
+else
+    echo -e "user already exists......$Y No need to create $N"
+fi
+
+mkdir /app
+VALIDATE $? "creating directory "
+
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
+VALIDATE $? "downloading user "
+
+rm -rf /app/*
+cd /app
+unzip -o /tmp/user.zip &>>$LOG_FILE
+VALIDATE $? "unzipping user "
+
+cd /app
+npm install
+VALIDATE $? "dependencies downloading "
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "daemon reload "
+
+systemctl enable user &>>$LOG_FILE
+VALIDATE $? "enabling user"
+
+systemctl start user &>>$LOG_FILE
+VALIDATE $? "user start "
+
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
